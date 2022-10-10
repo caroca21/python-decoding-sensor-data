45bf82caed5674e43ba142f20b810a9f7b5e0913
caroca21 <goncalocaroca@gmail.com>
2022-10-10 20:25:01 +0100

    Update house_info.py

--- sensor/house_info.py
+++ sensor/house_info.py
@@ -6,15 +6,15 @@ class HouseInfo(object):
     def __init__(self, data):
         self.data = data
 
-    def get_data_by_area(self, field, rec_area):
+    def get_data_by_area(self, field, rec_area=0):
         field_data = []
 
         for record in self.data:
             if rec_area == 0:
-                record[field].append(field_data)
+                field_data.append(record[field])
 
             elif rec_area == int(record['area']):
-                record[field].append(field_data)
+                field_data.append(record[field])
 
         return field_data
 
